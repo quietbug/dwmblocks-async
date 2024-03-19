7ebed5db0c2de85f76f550487c2f1b016464b36f
alie <agkuzt@gmail.com>
2024-03-19 06:26:37 +0300

    personal config

--- config.h
+++ config.h
@@ -1,16 +1,13 @@
 #define CMDLENGTH 60
-#define DELIMITER "  "
-#define CLICKABLE_BLOCKS
+#define DELIMITER " < "
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+    BLOCK("xkb-switch -W",      0,    0),
+    BLOCK("sb-net",             0,    0),
+    BLOCK("sb-mpd",             0,    0),
+    BLOCK("sb-uptime",          0,    0),
+    BLOCK("sb-volmon",          0,    0),
+    BLOCK("sb-cpu -d 1 -t 3",   0,    0),
+    BLOCK("sb-ram",             10,   0),
+    BLOCK("sb-timedate",        0,    0)
 };
